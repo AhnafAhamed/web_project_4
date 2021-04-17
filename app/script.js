85c804068ded99ead50870a6c205ae37a00cfe68
ahnaf ahamed <ahnafahamed4@gmail.com>
2021-04-17 15:14:01 +0530

    fix: second review fixes

--- app/script.js
+++ app/script.js
@@ -7,20 +7,23 @@ let inputName = document.querySelector(".popup__input_value_name");
 let inputAbout = document.querySelector(".popup__input_value_about");
 let submitBtn = document.querySelector(".popup__btn");
 
-inputName.value = profileTitle.textContent;
-inputAbout.value = profileTag.textContent;
-
-editBtn.addEventListener("click", function () {
+function popupOpen() {
   popup.classList.add("popup_open");
-});
-
-closeBtn.addEventListener("click", function () {
+  inputName.value = profileTitle.textContent;
+  inputAbout.value = profileTag.textContent;
+}
+function popupClose() {
   popup.classList.remove("popup_open");
-});
-
-submitBtn.addEventListener("click", function (event) {
+}
+function submitForm(event) {
   profileTitle.textContent = inputName.value;
   profileTag.textContent = inputAbout.value;
-  popup.classList.remove("popup_open");
+  popupClose();
   event.preventDefault();
-});
+}
+
+editBtn.addEventListener("click", popupOpen);
+
+closeBtn.addEventListener("click", popupClose);
+
+submitBtn.addEventListener("click", submitForm);
